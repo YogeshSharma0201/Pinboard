f686988b011400a256406ded0d5d65e08f88ae42
System <none>
2017-07-31 06:58:25 +0000

    🎐🛣 Updated with Glitch

--- config/config.js
+++
@@ -1,9 +0,0 @@
-var env = process.env.NODE_ENV || 'development';
-
-if (env === 'development' || env === 'test') {
-  var config = require('./config.json');
-
-  Object.keys(config).forEach((key)=>{
-    process.env[key] = config[key];
-  });
-}
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-require('./config/config.js');
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
